662487a360bae88f4a508eb08cc4afbed1107ff0
Prashant-Kashid1284 <prashantrkashid@gmail.com>
2025-03-15 10:02:53 +0530

    Menu Driven Program Using Function(Prime, Perfect Number or Not).

--- MENU.C
+++ MENU.C
@@ -1,61 +1,75 @@
-//Menu Driven Program using C for Arithmetic Operations(+,-,*,/)
+/*Menu Driven Program using C for Prime and Perfect Number or Not(Using FUnction)*/
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int a,b,ch;
-	float c;
+	int n,ch;
+	void prime(int x);
+	void perfect(int x);
 	clrscr();
-	printf("Enter Any Two  Nunmbers\n");
-	scanf("%d%d",&a,&b);
-	printf("Menu Driven Program using C\n");
+	printf("Enter any NUmber\n");
+	scanf("%d",&n);
+	printf("Menu Driven Program Using C\n");
 	do
 	{
-		printf("1:Addition\n");
-		printf("2:Subtraction\n");
-		printf("3:Multiplication\n");
-		printf("4:Division\n");
+		printf("Menu Includes\n");
+		printf("1:Prime\n");
+		printf("2:Perfect\n");
 		printf("0:Exit\n");
 		printf("Enter Your Choice\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1:	c=a+b;
-				printf("Addition=%0.0f\n",c);
+			case 1:	prime(n);
 				break;
-			case 2:	c=a-b;
-				printf("Subtraction=%0.0f\n",c);
+			case 2:	perfect(n);
 				break;
-			case 3:	c=a*b;
-				printf("Multiplication=%0.0f\n",c);
-				break;
-			case 4: if(b==0)
-				{
-					printf("Infinity\n");
-				}
-				else
-				{
-					c=(float)a/b;
-					printf("Division=%0.2f\n",c);
-				}
-				break;
-			case 0: exit(0);
+			case 0:	exit(0);
 			default:printf("Wrong Choice\n");
 		}
 	}while(ch!=0);
 	getch();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void perfect(int x)
+{
+	int a,sum=0;
+	a=1;
+	while(a<x)
+	{
+		if(x%a==0)
+		{
+			sum=sum+a;
+		}
+		a++;
+	}
+	if(sum==x)
+	{
+		printf("%d Number is Perfect\n",x);
+	}
+	else
+	{
+		printf("%d Number is Not Perfect\n",x);
+	}
+}
+void prime(int x)
+{
+	int a,flag=1;
+	a=1;
+	while(a<x)
+	{
+		if(x%a==0)
+		{
+			flag=0;
+			break;
+		}
+		a++;
+	}
+	if(flag==1)
+	{
+		printf("%d Number is Prime\n",x);
+	}
+	else
+	{
+		printf("%d Number is Not Prime\n",x);
+	}
+}
